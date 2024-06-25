ddbf46777cf5f18d4a83ff621b8df2ab66427e90
shinsukie <112907204+shinsukie@users.noreply.github.com>
2024-06-25 14:54:16 +0800

    first

---
+++ shoe/resources/views/home/new_arival.blade.php
@@ -0,0 +1,24 @@
+<section class="arrival_section">
+         <div class="container">
+            <div class="box">
+               <div class="arrival_bg_box">
+                  <img src="images/arrival-bg.png" alt="">
+               </div>
+               <div class="row">
+                  <div class="col-md-6 ml-auto">
+                     <div class="heading_container remove_line_bt">
+                        <h2>
+                           #NewArrivals
+                        </h2>
+                     </div>
+                     <p style="margin-top: 20px;margin-bottom: 30px;">
+                        Stay ahead of the trends with our latest collection of new arrivals at Shopify. From chic fashion pieces and innovative gadgets to stylish home decoration and must-have beauty products, our new arrivals are curated to keep you looking and feeling your best.
+                     </p>
+                     <a href="">
+                     Shop Now!
+                     </a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
---
+++ shoe/resources/views/home/slider.blade.php
@@ -0,0 +1,79 @@
+<section class="slider_section ">
+            <div class="slider_bg_box">
+               <img src="images/slider-bg.jpg" alt="">
+            </div>
+            <div id="customCarousel1" class="carousel slide" data-ride="carousel">
+               <div class="carousel-inner">
+                  <div class="carousel-item active">
+                     <div class="container ">
+                        <div class="row">
+                           <div class="col-md-7 col-lg-6 ">
+                              <div class="detail-box">
+                                 <h1>
+                                    <span>
+                                    
+                                    </span>
+                                    <br>
+                                    Sale 20% On Everything
+                                 </h1>
+                                 <p>
+                                    We're excited to announce our Super Summer Sale with an incredible 20% off on every single product in our store! Whether you've been eyeing that stylish new outfit, the latest gadget, or something special for your home, now is the perfect time to shop and save big.
+                                 </p>
+                                
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="carousel-item ">
+                     <div class="container ">
+                        <div class="row">
+                           <div class="col-md-7 col-lg-6 ">
+                              <div class="detail-box">
+                                 <h1>
+                                    <span>
+                                    
+                                    </span>
+                                    <br>
+                                    Enjoy Fast & Reliable Delivery on All Orders!
+                                 </h1>
+                                 <p>
+                                    At Shopify, we understand that when you shop online, you want your purchases to arrive quickly and safely. That's why we offer fast and reliable delivery on all orders, ensuring you receive your items in no time.
+                                 </p>
+                                 
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="carousel-item">
+                     <div class="container ">
+                        <div class="row">
+                           <div class="col-md-7 col-lg-6 ">
+                              <div class="detail-box">
+                                 <h1>
+                                    <span>
+                                    
+                                    </span>
+                                    <br>
+                                    Hassle-Free Returns and Refunds
+                                 </h1>
+                                 <p>
+                                    At Shopify, your satisfaction is our top priority. We understand that sometimes a purchase might not work out as expected, and that's why we offer a hassle-free return and refund policy to ensure a smooth shopping experience.
+                                 </p>
+                                 
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="container">
+                  <ol class="carousel-indicators">
+                     <li data-target="#customCarousel1" data-slide-to="0" class="active"></li>
+                     <li data-target="#customCarousel1" data-slide-to="1"></li>
+                     <li data-target="#customCarousel1" data-slide-to="2"></li>
+                  </ol>
+               </div>
+            </div>
+         </section>
